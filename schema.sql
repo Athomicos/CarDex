9f325ba5f2401866dd8edd02cb7fc6baae3527e1
Athomicos <theathomicos@gmail.com>
2026-09-04 00:17:08 +0200

    Schema.sql created

---
+++ schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE coches (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    marca TEXT NOT NULL,
+    modelo TEXT NOT NULL,
+    version TEXT NOT NULL,
+    año_inicio INTEGER,
+    año_fin INTEGER,
+    potencia_cv INTEGER,
+    motor TEXT,
+    0_100 REAL,
+    velocidad_maxima REAL,
+    otros_specs TEXT
+);
+
+CREATE TABLE usuaarios(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre_usuario TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+);
+
+
+CREATE TABLE avistamientos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario_id INTEGER NOT NULL,
+    coche_id INTEGER NOT NULL,
+    fecha DATETIME,
+    ubicacion TEXT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (coche_id) REFERENCES coches(id)
+    UNIQUE (usuario_id, coche_id)
+);
+
+CREATE TABLE fotos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    avistamiento_id INTEGER NOT NULL,
+    foto_path TEXT NOT NULL,
+    FOREIGN KEY (avistamiento_id) REFERENCES avistamientos(id)
+);
