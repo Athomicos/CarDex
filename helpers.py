bdf4c2f233999c14fea84506f4a046822b705287
Athomicos <theathomicos@gmail.com>
2026-09-04 16:33:54 +0200

    Create functions on helpers.py for format the kmh and cv values

--- helpers.py
+++ helpers.py
@@ -2,3 +2,11 @@ import requests
 
 from flask import redirect, render_template, session
 from functools import wraps
+
+def kmh(value):
+    """Format value as kilometers per hour."""
+    return f"{value:.1f} km/h"
+
+def cv(value):
+    """Format value as horsepower."""
+    return f"{value:.1f} cv"
