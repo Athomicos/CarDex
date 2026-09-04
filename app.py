ea63c2e440ab45a33e943d3647fccda107083fe1
Athomicos <theathomicos@gmail.com>
2026-09-04 16:58:46 +0200

    Añadir configs y generacion random de secret_key

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ from cs50 import SQL
 from flask import Flask
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, kmh, cv
+
+app = Flask(__name__)
+
+app.jinja_env.filters["kmh"] = kmh
+app.jinja_env.filters["cv"] = cv
