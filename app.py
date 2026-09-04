a544799d5e2f2ef72b56be2d7ec5fdb82c29beba
Athomicos <theathomicos@gmail.com>
2026-09-04 16:59:01 +0200

    Añadir configs y generacion random de secret_key

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from cs50 import SQL
 from flask import Flask
 from flask_session import Session
@@ -9,3 +11,20 @@ app = Flask(__name__)
 
 app.jinja_env.filters["kmh"] = kmh
 app.jinja_env.filters["cv"] = cv
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = os.urandom(24)
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///carspot.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
