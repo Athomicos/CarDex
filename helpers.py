e09be430b207262b09fc2a55604805c3a2e67ddb
Athomicos <theathomicos@gmail.com>
2026-09-04 16:59:35 +0200

    Add apology function

--- helpers.py
+++ helpers.py
@@ -3,6 +3,10 @@ import requests
 from flask import redirect, render_template, session
 from functools import wraps
 
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    return render_template("apology.html", top=code, bottom=message), code
+
 def kmh(value):
     """Format value as kilometers per hour."""
     return f"{value:.1f} km/h"
