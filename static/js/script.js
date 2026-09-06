aa976e7288dbb111ae5df2785b7fa5b366e6b80d
Athomicos <theathomicos@gmail.com>
2026-09-06 02:46:00 +0200

    script.js finished

--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,22 @@ document.getElementById("marca").addEventListener("change", function() {
     }
     selectModelo.disabled = false;
 });
+
+document.getElementById("modelo").addEventListener("change", async function() {
+    
+    const brand = document.getElementById("marca").value;
+    const model = this.value;
+
+    const respuesta = await fetch('/versions/${brand}/${model}');
+    const versions = await respuesta.json();
+
+    const selectVersion = document.getElementById("version");
+    selectVersion.innerHTML = "";
+    for (const v of versions) {
+        const option = document.createElement("option");
+        option.value = v.id;
+        option.textContent = v.version;
+        selectVersion.appendChild(option);
+    }
+    selectVersion.disabled = false;
+});
